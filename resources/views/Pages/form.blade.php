26bb4551bcb0ed1d66f10c4ed73233c728286c72
saleh forooze <salehforooz1383@gmail.com>
2024-02-19 20:36:01 +0330

    1_modify form page

--- resources/views/Pages/form.blade.php
+++ resources/views/Pages/form.blade.php
@@ -41,12 +41,12 @@
             </div>
         </div>
     
-        <div class="bg-white max-w-[576px] mt-[32px] mx-auto rounded-[12px] border-[1px] drop-shadow">
+        <div class="bg-white mobile:max-w-[90%] pc:max-w-[576px] tablet:max-w-[576px] mt-[32px] mx-auto rounded-[12px] border-[1px] drop-shadow">
             <div class="pt-[12px] m-2">
-                <div class="max-w-[500px] mx-auto">
+                <div class="mx-auto">
                     <p class="iransans-bold text-[15px]">مشخصات پویش</p>
                 </div>
-                <div class="w-full max-w-[500px] mx-auto">
+                <div class="w-full mx-auto">
                     <div class="flex pc:flex-col tablet:flex-col mobile:flex-col justify-center">
 
                         {{--section one--}}
@@ -56,7 +56,7 @@
                                     <label class="iransans-normal text-[13px] text-[#94a3b8]" for="campain-name">نام پویش:</label>
                                 </div>
                                 <div>
-                                    <input class="text-[13px] iransans-normal border-[1px] rounded-[8px] w-full h-[40px] mt-[3px] ps-[4px] pb-[4px] @error('name') border-red-500 @else border-[#cbd5e1] @enderror" type="text" id="campain-name" name="name" value="{{ old('name')}}">
+                                    <input class="text-[13px] iransans-normal border-[1px] rounded-[8px] w-full h-[40px] mt-[3px] ps-[4px] pb-[4px] focus:outline-dashed outline-1 outline-offset-1 focus:outline-[#cbd5e1] outline-white transition-all duration-300 ease-linear @error('name') border-red-500 @else border-[#cbd5e1] @enderror" type="text" id="campain-name" name="name" value="{{ old('name')}}">
                                     @error('name')
                                         <span class="text-[10px] text-red-500">{{ $message }}</span>
                                     @enderror
@@ -68,7 +68,7 @@
                                     <label class="iransans-normal text-[13px] text-[#94a3b8]" for="campain-start-date">شروع پویش:</label>
                                 </div>
                                 <div>
-                                    <input class="text-[13px] iransans-normal border-[1px] w-full h-[40px] rounded-[8px] mt-[3px] ps-[4px] pb-[4px] cursor-pointer @error('start-date') border-red-500 @else border-[#cbd5e1] @enderror" name="start-date" data-jdp value="{{ old('start-date') }}">
+                                    <input class="text-[13px] iransans-normal border-[1px] w-full h-[40px] rounded-[8px] mt-[3px] ps-[4px] pb-[4px] focus:outline-dashed outline-1 outline-offset-1 focus:outline-[#cbd5e1] outline-white transition-all duration-300 ease-linear cursor-pointer @error('start-date') border-red-500 @else border-[#cbd5e1] @enderror" name="start-date" data-jdp value="{{ old('start-date') }}">
                                     
                                     @error('start-date')
                                         <span class="text-[10px] text-red-500">{{ $message }}</span>
@@ -81,7 +81,7 @@
                                     <label class="iransans-normal text-[13px] text-[#94a3b8]" for="campain-end-date">خاتمه پویش:</label>
                                 </div>
                                 <div>
-                                    <input class="text-[13px] iransans-normal border-[1px] w-full h-[40px] rounded-[8px] mt-[3px] ps-[4px] pb-[4px] cursor-pointer @error('start-date') border-red-500 @else border-[#cbd5e1] @enderror" name="finish-date" data-jdp value="{{ old('finish-date') }}">
+                                    <input class="text-[13px] iransans-normal border-[1px] w-full h-[40px] rounded-[8px] mt-[3px] ps-[4px] pb-[4px] focus:outline-dashed outline-1 outline-offset-1 focus:outline-[#cbd5e1] outline-white transition-all duration-300 ease-linear cursor-pointer @error('start-date') border-red-500 @else border-[#cbd5e1] @enderror" name="finish-date" data-jdp value="{{ old('finish-date') }}">
                                     @error('finish-date')
                                         <span class="text-[10px] text-red-500">{{ $message }}</span>
                                     @enderror
@@ -98,7 +98,7 @@
                                         <label class="iransans-normal text-[13px] text-[#94a3b8]" for="campain-location">محدوده پویش:</label>
                                     </div>
                                     <div>
-                                        <input class="text-[13px] iransans-normal border-[1px] w-full h-[40px] rounded-[8px] mt-[3px] ps-[4px] pb-[4px] @error('geometry-location') border-red-500 @else border-[#cbd5e1] @enderror" type="text" id="campain-location" name="geometry-location" value="{{ old('geometry-location') }}">
+                                        <input class="text-[13px] iransans-normal border-[1px] w-full h-[40px] rounded-[8px] mt-[3px] ps-[4px] pb-[4px] focus:outline-dashed outline-1 outline-offset-1 focus:outline-[#cbd5e1] outline-white transition-all duration-300 ease-linear @error('geometry-location') border-red-500 @else border-[#cbd5e1] @enderror" type="text" id="campain-location" name="geometry-location" value="{{ old('geometry-location') }}">
                                         @error('geometry-location')
                                             <span class="text-[10px] text-red-500">{{ $message }}</span>
                                         @enderror
@@ -109,22 +109,24 @@
                                     <div class="flex flex-col items-center justify-center">
                                         <span class="iransans-normal text-[13px]">تصویر پویش:</span>
                                     </div>
-                                    <div>
+                                    <div class="flex flex-col justify-center items-center">
                                         <label class="iransans-normal text-[14px] bg-[#22c55e] text-white p-2 rounded-[12px] pc:hover:brightness-110 transition-all duration-300 cursor-pointer" for="campain-picture">بارگذاری پوستر</label>
-                                            <input class="text-[13px] iransans-normal border-[1px] w-full h-[40px] border-black rounded-[8px] mt-[3px] ps-[4px] pt-[3px]" type="file" id="campain-picture" name="campain-poster"  hidden>
-                                        @error('campain-poster')
-                                            <span class="text-[10px] text-red-500">{{ $message }}</span>
-                                        @enderror
+                                        <input class="text-[13px] iransans-normal border-[1px] w-full h-[40px] border-black rounded-[8px] mt-[3px] ps-[4px] pt-[3px]" type="file" id="campain-picture" name="campain-poster"  hidden>
+                                        <div>
+                                        </div>
+                                            @error('campain-poster')
+                                                        <span class="text-[10px] text-red-500">{{ $message }}</span>
+                                            @enderror
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
     
                             <div class="w-full mx-[20px]">
                                 <div>
                                     <label class="iransans-normal text-[13px] text-[#94a3b8]" for="campain-description">توضیحات پویش:</label>
                                 </div>
                                 <div>
-                                    <textarea class="text-[13px] iransans-normal border-[1px] w-full h-[200px] rounded-[8px] mt-[3px] ps-[4px] pt-[3px] @error('description') border-red-500 @else border-[#cbd5e1] @enderror" name="description" id="campain-descrption" cols="10" rows="60"></textarea>
+                                    <textarea class="text-[13px] iransans-normal border-[1px] w-full h-[200px] rounded-[8px] mt-[3px] ps-[4px] pt-[3px] focus:outline-dashed outline-1 outline-offset-1 focus:outline-[#cbd5e1] outline-white transition-all duration-300 ease-linear @error('description') border-red-500 @else border-[#cbd5e1] @enderror" name="description" id="campain-descrption" cols="10" rows="60"></textarea>
                                     @error('description')
                                         <span class="text-[10px] text-red-500">{{ $message }}</span>
                                     @enderror
@@ -136,11 +138,11 @@
                 </div>
             </div>
         
-            <div class="mt-[12px] border-t-2 pt-[12px] m-2 max-w-[500px] mx-auto">
-                <div class="w-full max-w-[500px] mx-auto">
-                    <p class="iransans-bold text-[15px]">اطلاعات آغازگر پویش</p>
+            <div class="mt-[12px] border-t-2 pt-[12px] m-2 max-w-[97%] mx-auto">
+                <div class="w-full mx-auto">
+                    <p class="iransans-bold text-[15px]">آغازگر پویش</p>
                 </div>
-                <div class="w-full max-w-[500px] mx-auto">
+                <div class="w-full mx-auto">
                     <div class="flex pc:flex-row tablet:flex-row mobile:flex-col justify-center items-center">
     
                         <div class="pc:w-1/3 mobile:w-full">
@@ -148,7 +150,7 @@
                                 <label class="iransans-normal text-[13px] text-[#94a3b8]" for="firstname">نام:</label>
                             </div>
                             <div>
-                                <input class="text-[13px] iransans-normal border-[1px] w-full h-[40px] rounded-[8px] mt-[3px] ps-[4px] pb-[4px] @error('leader-firstname') border-red-500 @else border-[#cbd5e1] @enderror" type="text" id="firstname" name="leader-firstname" value="{{ old('leader-firstname') }}">
+                                <input class="text-[13px] iransans-normal border-[1px] w-full h-[40px] rounded-[8px] mt-[3px] ps-[4px] pb-[4px] focus:outline-dashed outline-1 outline-offset-1 focus:outline-[#cbd5e1] outline-white transition-all duration-300 ease-linear @error('leader-firstname') border-red-500 @else border-[#cbd5e1] @enderror" type="text" id="firstname" name="leader-firstname" value="{{ old('leader-firstname') }}">
                                 @error('leader-firstname')
                                     <span class="text-[10px] text-red-500">{{ $message }}</span>
                                 @enderror
@@ -160,7 +162,7 @@
                                 <label class="iransans-normal text-[13px] text-[#94a3b8]" for="lastname">نام خانوادگی:</label>
                             </div>
                             <div>
-                                <input class="text-[13px] iransans-normal border-[1px] w-full h-[40px] rounded-[8px] mt-[3px] ps-[4px] pb-[4px] @error('leader-lastname') border-red-500 @else border-[#cbd5e1] @enderror" type="text" id="lastname" name="leader-lastname" value="{{ old('leader-lastname') }}">
+                                <input class="text-[13px] iransans-normal border-[1px] w-full h-[40px] rounded-[8px] mt-[3px] ps-[4px] pb-[4px] focus:outline-dashed outline-1 outline-offset-1 focus:outline-[#cbd5e1] outline-white transition-all duration-300 ease-linear @error('leader-lastname') border-red-500 @else border-[#cbd5e1] @enderror" type="text" id="lastname" name="leader-lastname" value="{{ old('leader-lastname') }}">
                                 @error('leader-lastname')
                                     <span class="text-[10px] text-red-500">{{ $message }}</span>
                                 @enderror
@@ -172,7 +174,7 @@
                                 <label class="iransans-normal text-[13px] text-[#94a3b8]" for="phone">شماره تلفن:</label>
                             </div>
                             <div>
-                                <input class="text-[13px] border-[1px] w-full h-[40px] rounded-[8px] mt-[3px] ps-[4px] pb-[4px] @error('leader-phone') border-red-500 @else border-[#cbd5e1] @enderror" type="text" id="phone" name="leader-phone"  value="{{ old('leader-phone') }}">
+                                <input class="text-[13px] border-[1px] w-full h-[40px] rounded-[8px] mt-[3px] ps-[4px] pb-[4px] focus:outline-dashed outline-1 outline-offset-1 focus:outline-[#cbd5e1] outline-white transition-all duration-300 ease-linear @error('leader-phone') border-red-500 @else border-[#cbd5e1] @enderror" type="text" id="phone" name="leader-phone"  value="{{ old('leader-phone') }}">
                                 @error('leader-phone')
                                     <span class="text-[10px] text-red-500">{{ $message }}</span>
                                 @enderror
@@ -182,7 +184,7 @@
                 </div>
             </div>
     
-            <div class="flex flex-row justify-center items-center mx-auto w-[87%] my-5 pc:hover:brightness-110 transition-all duration-300">
+            <div class="flex flex-row justify-center items-center mx-auto max-w-[97%] my-5 pc:hover:brightness-110 transition-all duration-300">
                 <button class="bg-[#22c55e] text-white text-[14px] p-2 rounded-[12px] w-full h-[48px] iransans-normal" type="submit">
                     ثبت پویش
                 </button>
